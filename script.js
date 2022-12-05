b61fd400d2b1674a10bd6101cc021934889864d3
Anschi <anna-sophie@easysaas.at>
2022-12-05 11:38:46 +0100

    add function

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-/*
-"How you doin'?"
-"Joey doesn’t share food!”
-"Could I BE wearing any more clothes?!”
-"I’m not great at the advice. Can I interest you in a sarcastic comment?”
-“We were on a break!”
-“Unagi.”
-“Pivot!”
-“Oh, I wish I could, but I don’t want to."
-"Oh I'm sorry. Did my back hurt your knife?"
-"I KNOW!"
-"OH MY GOD."
-**/
-
 const mixedMessages = 
 ["How you doin?", 
 "Could I BE wearing any more clothes?!",
