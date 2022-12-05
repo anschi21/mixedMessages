9c37ffa227b7d236da0f3d0f8139a343cbf3871b
Anschi <anna-sophie@easysaas.at>
2022-12-05 09:56:16 +0100

    init

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+"How you doin'?"
+"Joey doesn’t share food!”
+"Could I BE wearing any more clothes?!”
+"I’m not great at the advice. Can I interest you in a sarcastic comment?”
+“We were on a break!”
+“Unagi.”
+“Pivot!”
+“Oh, I wish I could, but I don’t want to."
+"Oh I'm sorry. Did my back hurt your knife?"
+"I KNOW!"
+"OH MY GOD."
+**/
+
+const mixedMessages = 
+["How you doin?", 
+"Could I BE wearing any more clothes?!",
+"I am not great at the advice. Can I interest you in a sarcastic comment?",
+"We were on a break!",
+"Unagi.",
+"Pivot!",
+"Oh, I wish I could, but I don’t want to.",
+"Oh I'm sorry. Did my back hurt your knife?",
+"I KNOW!",
+"OH MY GOD."]
